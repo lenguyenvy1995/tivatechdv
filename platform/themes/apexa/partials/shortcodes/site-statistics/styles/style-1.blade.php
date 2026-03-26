c6a4fd9516e08b05f8dbc7a14d3f71f90165d104
lenguyenvy1995 <42138419+lenguyenvy1995@users.noreply.github.com>
2026-03-26 14:38:41 +0700

    done site staticstics style 1 2

--- platform/themes/apexa/partials/shortcodes/site-statistics/styles/style-1.blade.php
+++ platform/themes/apexa/partials/shortcodes/site-statistics/styles/style-1.blade.php
@@ -1,4 +1,4 @@
-<section class="counter-area shortcode-site-statistics" @style($variablesStyle)>
+<section class=" shortcode-site-statistics-style-1 counter-area shortcode-site-statistics shortcode-tivatech" @style($variablesStyle)>
     <div class="container">
         <div class="row justify-content-center">
             {!! Theme::partial('shortcodes.site-statistics.partials.items', compact('tabs')) !!}
--- platform/themes/apexa/partials/shortcodes/site-statistics/styles/style-2.blade.php
+++ platform/themes/apexa/partials/shortcodes/site-statistics/styles/style-2.blade.php
@@ -1,35 +1,30 @@
-<section class="journey_area-seven shortcode-site-statistics" @style($variablesStyle)>
+<section class=" shortcode-site-statistics-style-2 shortcode-site-statistics shortcode-tivatech" @style($variablesStyle)>
     <div class="container">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xl-6 col-lg-8">
-                    <div class="section-title text-center mb-40 tg-heading-subheading animation-style3">
-                        @if ($subtitle = $shortcode->subtitle)
-                            <span class="sub-title">{!! BaseHelper::clean($subtitle) !!}</span>
-                        @endif
 
-                        @if ($title = $shortcode->title)
-                            <h2 class="title tg-element-title">{!! BaseHelper::clean($title) !!}</h2>
-                        @endif
+        <div class="row justify-content-center">
+            <div class="col-xl-8 col-lg-8">
+                <div class="section-title text-center mb-40 tg-heading-subheading animation-style3">
+                    @if ($subtitle = $shortcode->subtitle)
+                        <span class="sub-title">{!! BaseHelper::clean($subtitle) !!}</span>
+                    @endif
 
-                        @if ($description = $shortcode->description)
-                            <p class="mb-25">{!! BaseHelper::clean($description) !!}</p>
-                        @endif
+                    @if ($title = $shortcode->title)
+                        <h2 class="title tg-element-title">{!! BaseHelper::clean($title) !!}</h2>
+                    @endif
 
-                        @if (($buttonLabel = $shortcode->button_label) && ($buttonUrl = $shortcode->button_url))
-                            <a
-                                href="{{ $buttonUrl }}"
-                                class="btn"
-                                data-aos="fade-up"
-                                data-aos-delay="600"
-                            >
-                                {!! BaseHelper::clean($buttonLabel) !!}
-                            </a>
-                        @endif
-                    </div>
+                    @if ($description = $shortcode->description)
+                        <p class="mb-25">{!! BaseHelper::clean($description) !!}</p>
+                    @endif
+
+                    @if (($buttonLabel = $shortcode->button_label) && ($buttonUrl = $shortcode->button_url))
+                        <a href="{{ $buttonUrl }}" class="btn" data-aos="fade-up" data-aos-delay="600">
+                            {!! BaseHelper::clean($buttonLabel) !!}
+                        </a>
+                    @endif
                 </div>
             </div>
         </div>
+
         <div class="box-counter-home7">
             <div class="row justify-content-center">
                 {!! Theme::partial('shortcodes.site-statistics.partials.items', compact('tabs')) !!}
